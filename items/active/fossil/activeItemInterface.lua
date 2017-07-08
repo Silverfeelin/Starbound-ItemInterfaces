714539863f86e799c850f14c4be12e5f742abb36
Silverfeelin <Silverfeelin@gmail.com>
2017-07-08 20:37:24 +0200

    primary/alt & table parameters

--- items/active/fossil/activeItemInterface.lua
+++ items/active/fossil/activeItemInterface.lua
@@ -5,30 +5,76 @@
   The item will then open the interface from the path of the "itemInterface" parameter when activated.
 
   Note that this file should be required in the init function of a vanilla active item script to prevent issues on servers.
-  The first line in function init():
-  if (require "/items/active/fossil/activeItemInterface.lua") then return end
+  The first lines in function init():
+  require "/items/active/fossil/activeItemInterface.lua"
+  if itemInterface then return end
 ]]
 
-local interfacePath = config.getParameter("itemInterface")
-local interfaceType = config.getParameter("itemInterfaceType") or "ScriptConsole"
+local version = "1.3.1"
+local logError = function(msg, ...)
+  sb.logError("ItemInterfaces %s: " .. msg, version, ...)
+end
 
-local holding = config.getParameter("itemInterfaceHolding")
-local requiredShiftHeld = config.getParameter("itemInterfaceShiftHeld")
+-- Defaults
+local default = {
+  type = "ScriptPane",
+  holding = true
+}
 
-if type(holding) == "boolean" then
-  activeItem.setHoldingItem(holding)
+-- Read root / table parameters
+local interfaceConfig = config.getParameter("itemInterface")
+local interfacePath, interfaceType, interfaceHolding, interfaceRequiresShift, interfacePrimary, interfaceAlt
+
+if type(interfaceConfig) == "string" then
+  interfacePath = interfaceConfig
+  interfaceType = config.getParameter("itemInterfaceType")
+  interfaceHolding = config.getParameter("itemInterfaceHolding")
+  interfaceRequiresShift = config.getParameter("itemInterfaceShiftHeld")
+  interfacePrimary = config.getParameter("itemInterfacePrimary")
+  interfaceAlt = config.getParameter("itemInterfaceAlt")
+else
+  interfacePath = interfaceConfig.path
+  interfaceType = interfaceConfig.type
+  interfaceHolding = interfaceConfig.holding
+  interfaceRequiresShift = interfaceConfig.shift
+  interfacePrimary = interfaceConfig.primary
+  interfaceAlt = interfaceConfig.alt
+
+  if not interfacePath then
+    logError("Could not load an item interface as no 'path' is defined. This item will behave as a regular fossil brush.")
+  end
+end
+
+-- Defaults
+if type(interfaceType) ~= "string" then interfaceType = default.type end
+if type(interfaceHolding) ~= "boolean" then interfaceHolding = default.holding end
+
+-- Show or hide item.
+if type(interfaceHolding) == "boolean" then
+  activeItem.setHoldingItem(interfaceHolding)
+end
+
+-- Determine primary / alt fire requirement.
+if interfacePrimary == nil and interfaceAlt == nil then -- None -> Both
+  interfacePrimary, interfaceAlt = true, true
+elseif interfaceAlt == nil then -- Primary false -> Alt true. Primary true -> Alt false.
+  interfaceAlt = not interfacePrimary
+elseif interfacePrimary == nil then -- Alt false -> Primary true. Alt true -> Primary false.
+  interfacePrimary = not interfaceAlt
+elseif not interfacePrimary and not interfaceAlt then -- Both false -> Error.
+  logError("Primary and Alt are both set to false. This item interface can not be opened.")
 end
 
 --[[
   Check if this item is a valid Item Interfaces active item.
 ]]
 if not interfacePath then
-  -- Not valid; prevent this script from initializing.
+  -- Not valid; prevent this script from initializing (before itemInterface is defined).
   return
 else
   -- Check if the file exists.
   if not pcall(function() root.assetJson(interfacePath) end) then
-    sb.logError("ItemInterfaces: Could not load the interface '%s'. This item will behave as a regular fossil brush.", interfacePath)
+    logError("Could not load the interface '%s'. This item will behave as a regular fossil brush.", interfacePath)
     return
   end
 end
@@ -37,19 +83,7 @@ end
   Item Interface table. Callbacks are stored here.
   You can use this table to store variables and functions, but feel free to define your own tables and variables.
 ]]
-itemInterface = {
-  version = "1.3.0.1"
-}
-
---[[
-  Update callback. Called every script.updateDt game ticks.
-  @param dt - Time difference since last tick. Should always equal script.updateDt / 60.
-  @param fireMode - "none", "primary" or "alt", indicating which mouse button is held down.
-  @param shiftHeld - Value indicating whether the shift key is held down or not.
-]]
-function itemInterface.update(dt, fireMode, shiftHeld)
-
-end
+itemInterface = { }
 
 --[[
   Activation callback. Called when activating the item, regardless of the button used.
@@ -59,9 +93,10 @@ end
 ]]
 function itemInterface.activate(fireMode, shiftHeld)
   -- Does shiftHeld match 'itemInterfaceShiftHeld'? Only matters if the parameter is defined on the item.
-  if requiredShiftHeld ~= nil and requiredShiftHeld ~= shiftHeld then
-    return
-  end
+  if interfaceRequiresShift ~= nil and interfaceRequiresShift ~= shiftHeld then return end
+
+  if fireMode == "primary" and not interfacePrimary then return end
+  if fireMode == "alt" and not interfaceAlt then return end
 
   local interfaceConfig = root.assetJson(interfacePath)
 
@@ -77,17 +112,11 @@ function itemInterface.activate(fireMode, shiftHeld)
   activeItem.interact(interfaceType, interfaceConfig)
 end
 
---[[
-  Uninit callback. Called when dying, reloading, leaving the current planet, etc.
-]]
-function itemInterface.uninit()
-
-end
-
 -- Code that makes the active item use the callbacks from this script rather than the original fossil brush callbacks.
-update = itemInterface.update
 activate = itemInterface.activate
-uninit = itemInterface.uninit
+init = function() end
+update = function(args) end
+uninit = function() end
 
 -- Because Starbound doesn't support return values when requiring scripts, the existance of the "itemInterface" table should be checked.
--- The table is not defined if the item is not an ItemInterfaces item.
+-- The table is not defined if the item is not a valid ItemInterfaces item.
